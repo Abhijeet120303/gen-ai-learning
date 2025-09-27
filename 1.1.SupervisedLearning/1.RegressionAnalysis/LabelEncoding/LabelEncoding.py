158b4b8ccc3ca27df8769a40fdeadb25789301ee
Abhijeet Nirphal <nirphalabhijeet72@gmail.com>
2025-09-27 16:35:34 +0530

    feat: Implement label encoding
    * Encode categorical columns using LabelEncoder
    * Prepare data for machine learning models

---
+++ 1.1.SupervisedLearning/1.RegressionAnalysis/LabelEncoding/LabelEncoding.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv('salary_data.csv')
+
+X = df.drop('Salary', axis=1)
+y = df['Salary']
+
+encoder = LabelEncoder()
+X['Title'] = encoder.fit_transform(X['Title'])
+
+model = LinearRegression()
+model.fit(X, y)
+
+output = model.predict(pd.DataFrame([[1, 8]], columns=['Title', 'Experience']))
+
+print(output)
